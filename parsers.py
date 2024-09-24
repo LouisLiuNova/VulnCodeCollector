94f658653c58d0bc5d87801861352368bd0170b7
LouisLiu <louisliunova@icloud.com>
2024-09-24 10:57:51 +0800

    fix: regex to support both http and https

--- parsers.py
+++ parsers.py
@@ -14,19 +14,19 @@ def register_parser(name):
 
 
 qemu_parser = BaseParser("qemu", "qemu", "qemu", [
-                         r"http:\/\/git\.qemu-project\.org\/\?p=qemu\.git;a=(?:commit|commitdiff);h=[a-f0-9]{40}", r'http:\/\/git\.qemu\.org/\?p=qemu\.git;a=(?:commit|commitdiff);h=[0-9a-f]{40}$'])
+                         r"https?:\/\/git\.qemu-project\.org\/\?p=qemu\.git;a=(?:commit|commitdiff);h=[a-f0-9]{40}", r'https?:\/\/git\.qemu\.org/\?p=qemu\.git;a=(?:commit|commitdiff);h=[0-9a-f]{40}$'])
 register_parser("qemu")(qemu_parser)
 linux_parser = BaseParser("linux", "torvalds", "linux", [
-                          r'http:\/\/git\.kernel\.org\/cgit\/linux\/kernel\/git\/torvalds\/linux\.git\/commit\/\?id=[0-9a-f]{40}', r'http:\/\/git\.kernel\.org\/\?p=linux\/kernel\/git\/torvalds\/linux-2\.6\.git;a=commit;h=[0-9a-f]{40}'])
+                          r'https?:\/\/git\.kernel\.org\/cgit\/linux\/kernel\/git\/torvalds\/linux\.git\/commit\/\?id=[0-9a-f]{40}', r'https?:\/\/git\.kernel\.org\/\?p=linux\/kernel\/git\/torvalds\/linux-2\.6\.git;a=commit;h=[0-9a-f]{40}'])
 register_parser("linux")(linux_parser)
 ffmpeg_parser = BaseParser("ffmpeg", "FFmpeg", "FFmpeg", [
-                           r'http:\/\/git\.videolan\.org\/\?p=ffmpeg\.git;a=(?:commit|commitdiff);h=[0-9a-f]{40}'])
+                           r'https?:\/\/git\.videolan\.org\/\?p=ffmpeg\.git;a=(?:commit|commitdiff);h=[0-9a-f]{40}'])
 register_parser("ffmpeg")(ffmpeg_parser)
 freetype2_parser = BaseParser("freetype2", "freetype", "freetype", [
-                              r'http:\/\/git\.savannah\.gnu\.org\/cgit\/freetype\/freetype2\.git\/commit\/\?id=[a-fA-F0-9]{40}'])
+                              r'https?:\/\/git\.savannah\.gnu\.org\/cgit\/freetype\/freetype2\.git\/commit\/\?id=[a-fA-F0-9]{40}'])
 register_parser("freetype2")(freetype2_parser)
 openssl_parser = BaseParser("openssl", "openssl", "openssl", [
-                            r'http:\/\/git\.openssl\.org\/gitweb\/\?p=openssl\.git;a=(?:commit|commitdiff);h=[a-f0-9]{40}'])
+                            r'https?:\/\/git\.openssl\.org\/gitweb\/\?p=openssl\.git;a=(?:commit|commitdiff);h=[a-f0-9]{40}'])
 register_parser("openssl")(openssl_parser)
 
 

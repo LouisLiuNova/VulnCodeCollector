47ee961bc724562744b972bfa78c755538b3aa1d
LouisLiu <louisliunova@icloud.com>
2024-09-24 16:39:10 +0800

    chore: update inputs

--- export.py
+++ export.py
@@ -2,3 +2,15 @@
 to expoert the data from the database to a csv file
 
 Export the data from the database to a csv file with the given columns."""
+
+import pathlib
+from loguru import logger
+
+
+def export(output_path: pathlib.Path):
+    """ Main function to export the data to the remote API. Exported data should be in CSV and has the following columns:
+    - CVE Number
+    - github commit URL
+    - vendor/product name
+    - etc."""
+    logger.info(f"Exporting data to {output_path}")
